7b4047c6f168bbb900779b0684602b336a7fd630
Jefferson <jeffersonraimon@gmail.com>
2021-12-07 17:15:42 -0300

    exercises

---
+++ C++/aulas.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+void somador(){
+    int number1, number2, soma = 0;
+
+    cin >> number1 >> number2;
+
+    soma = number1 + number2;
+
+}
+
+
+int main(){
+
+    somador();
+
+    cout << "the sum of the numbers is: " << endl;
+    
+    return 0;
+}   
---
+++ C/exer02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+float grade1, grade2;
+printf("type the first grade: ");
+scanf("%f",&grade1);
+printf("type the second grade: ");
+scanf("%f",&grade2);
+printf("Average = %2.f\n", (grade2 + grade1)/2);
+
+
+
+
+
+
+}
